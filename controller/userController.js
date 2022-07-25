ca3c3245bafd28e030bae9048c15f297ace01e4f
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2022-07-26 01:06:48 +0530

    added userUpdate

--- controller/userController.js
+++ controller/userController.js
@@ -1,72 +1,78 @@
 //registration
 const db = require("../database/db");
 exports.userRegistration = (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const centre = req.body.centre;
-    const password = req.body.password;
+  const username = req.body.username;
+  const email = req.body.email;
+  const centre = req.body.centre;
+  const password = req.body.password;
 
-    db.query(
-        "INSERT INTO registration(username,email,centre,password) VALUES(?,?,?,?)", [username, email, centre, password],
+  db.query(
+    "INSERT INTO registration(username,email,centre,password) VALUES(?,?,?,?)",
+    [username, email, centre, password],
+    (err, result) => {
+      db.query(
+        "SELECT * from registration WHERE email = ?",
+        [email],
         (err, result) => {
-            console.log(err);
-            res.send(result);
+          res.user = result;
+          db.query(
+            "INSERT INTO  alumniupdate (aid) VALUES(?)",
+            [result[0].id],
+            (err, result) => {
+              res.send(result);
+            }
+          );
+          console.log(err);
+          res.send(result);
         }
-    );
+      );
+    }
+  );
+};
+
+//updateUserProfile
+
+exports.userUpdate = (req, res) => {
+  const { id } = req.params;
+  console.log("user", req.body.userData);
+  db.query(
+    "UPDATE `alumniupdate` SET ? WHERE aid = ?",
+    [req.body.userData, id],
+    (err, result) => {
+      console.log(err);
+      res.send(result);
+    }
+  );
 };
 
 //login
 exports.userLogin = (req, res) => {
-    db.query(
-        "SELECT * from registration WHERE email = ? and password = ?", [req.body.email, req.body.password],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                {
-                    console.log("Done");
-                    res.send(result);
-                }
-            }
+  db.query(
+    "SELECT * from registration WHERE email = ? and password = ?",
+    [req.body.email, req.body.password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        {
+          console.log("Done");
+          res.send(result);
         }
-    );
+      }
+    }
+  );
 };
 
-// userUpdateProfile
-exports.userUpdateDetails = (req, res) => {
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const centre = req.body.centre;
-    const course = req.body.course;
-    const contact = req.body.contact;
-    const linkedin = req.body.linkedin;
-    const address = req.body.address;
-    const state = req.body.state;
-    const occupation = req.body.occupation;
-    const currentwork = req.bosy.currentwork;
-    const courseyear = req.body.courseyear;
-    const pic = req.body.pic;
-    db.query(
-        "INSERT INTO alumniupdate(firstname, lastname, centre, course, contact, linkedin, address, state, occupation, currentwork, courseyear, pic) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)" [
-            (firstname,
-                lastname,
-                centre,
-                course,
-                contact,
-                linkedin,
-                address,
-                state,
-                occupation,
-                currentwork,
-                courseyear,
-                pic)
-        ],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        }
-    );
+exports.getUser = (req, res) => {
+  const { id } = req.params;
+  db.query("SELECT * from alumniupdate WHERE aid = ?", [id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      {
+        console.log("Done");
+        res.send(result);
+      }
+    }
+  });
 };
